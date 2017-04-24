033fe2094435ef3dff6db4899a83f11e0e0495e8
Benjamin Hetz <Benjamin Hetz@DESKTOP-N7IHLRU.Home>
2017-04-24 02:13:50 -0600

    Finished project
    Successfully parses regExes that have parenthetical expressions, unions,
    and Kleene Stars

--- re/RE.java
+++ re/RE.java
@@ -4,6 +4,12 @@ import fa.State;
 import fa.nfa.NFA;
 import fa.nfa.NFAState;
 
+/**
+ * Parses a regular expression string and converts it into
+ * an NFA.
+ * @author Benjamin Hetz
+ *
+ */
 public class RE implements REInterface {
 
         private String regEx;
@@ -13,57 +19,124 @@ public class RE implements REInterface {
         public RE(String regEx)
         {
                 this.regEx = regEx;
-                nfa = new NFA();
                 counter = 0;
         }
+        /**
+         * Parses the regEx string into an NFA
+         */
         public NFA getNFA()
         {
         	nfa = parseRegex();
                 return nfa;
         }
+        /**
+         * Parses a regEx consisting of a term, or a term
+         * followed by a '|' and another regEx
+         * @return parsed regEx
+         */
         private NFA parseRegex()
         {
                 NFA reg = null;
                 NFA term = parseTerm();
                 if (more() && peek() == '|') {
                         eat('|');
-                        NFA regEx = parseRegex();
-                        reg = union(regEx, term);
+                        reg = parseRegex();
+                        term = union(reg, term);
+                        return term;
                 }
-        	return reg;	
+        	return term;	
         }
+        /**
+         * Parses a term consisting of a factor
+         * or no factors
+         * 
+         * Since the algorithm is recursive, I had to check if
+         * the NFA was already there, in which case I should concatenate
+         * 
+         * @return The term parsed into an NFA
+         */
         private NFA parseTerm()
         {
-                while(peek() != ')' && peek() != '|' && more()) {
-                        NFA n = parseFactor();
+                NFA n = null;
+                while(more() && peek() != ')' && peek() != '|') {
+                        if (n == null) {
+                                n = parseFactor();
+                        } else {
+                                n = concatenate(n, parseFactor());
+                        }
                 }
-                return null;
+                return n;
         }
+        /**
+         * Parses a factor consisting of a base
+         * optionally followed by a Kleene Star
+         * 
+         * @return The factor parsed into an NFA
+         */
         private NFA parseFactor()
         {
                 NFA n = parseBase();
                 while(more() && peek() == '*') {
                         eat('*');
-                        
+                        n = repeat(n);
                 }
-                return null;
+                return n;
         }
+        /**
+         * Parses a base consisting of a char or a
+         * '(' followed by a full regEx followed by a ')'
+         * 
+         * @return the parsed base
+         */
         private NFA parseBase()
         {
                 if (peek() == '(') {
                         eat('(');
-                        parseRegex();
+                        NFA n = parseRegex();
                         eat(')');
-                        return null;
+                        return n;
                 } else {
-                        nfa.addState(String.valueOf(counter));
+                        //Make a two state, 1 transition NFA for this char
+                        NFA base = new NFA();
+                        String startStateName = String.valueOf(counter);
+                        base.addStartState(startStateName);
+                        counter++;
+                        String finalStateName = String.valueOf(counter);
+                        base.addFinalState(finalStateName);
+                        counter++;
+                        base.addTransition(startStateName, next(), finalStateName);
+                        return base;
                 }
-                return null;
         }
+        /**
+         * Consumes the next char from input
+         * 
+         * @return the next char in the input string
+         */
+        private char next()
+        {
+                char c = regEx.charAt(0);
+                regEx = regEx.substring(1);
+                return c;
+        }
+        /**
+         * Checks out the next char from input
+         * this method does not consume the char
+         * 
+         * @return the next char in the input string
+         */
         private char peek()
         {
                 return regEx.charAt(0);
         }
+        /**
+         * Checks if the next char in input matches
+         * c, and consumes it if it does.
+         * 
+         * Error out if we find something we don't expect
+         * 
+         * @param c the char to check
+         */
         private void eat(char c)
         {
                 if (peek() == c) {
@@ -74,32 +147,43 @@ public class RE implements REInterface {
                 }
                 return;
         }
+        /**
+         * Returns whether the input has more chars
+         * 
+         * @return boolean value of whether input has more chars or not
+         */
         private boolean more()
         {
                 return regEx.length() > 0;
         }
         /**
-         * Generates the new NFA formed by concatenating n onto the end of our current
-         * NFA
+         * Generates the new NFA formed by concatenating n2 onto the end of n1
          * 
-         * @param n the nfa to concatenate onto the one stored in our object
-         * @return the new NFA formed by concatenating n onto the end of the stored NFA
+         * @param n2 the nfa to concatenate onto n1
+         * @param n1 the nfa to be concatenated on to
+         * 
+         * @return the new NFA formed by concatenating n2 onto the end of n1
          */
-        private void concatenate(NFA n)
+        private NFA concatenate(NFA n1, NFA n2)
         {
-                for (State s: nfa.getFinalStates()) {
-                        //Set final states of nfa to not final
+                for (State s: n1.getFinalStates()) {
+                        //Set final states of NFA to not final
                         //Add e transitions from old final states to start state of n
                         NFAState nS = ((NFAState)s);
                         nS.setNonFinal();
-                        nS.addTransition('e', (NFAState)n.getStartState());    
+                        nS.addTransition('e', (NFAState)n2.getStartState());    
                 }
+                //Add alphabet and states from n2 into n1
+                n1.addNFAStates(n2.getStates());
+                n1.addAbc(n2.getABC());
+                return n1;
         }
         
         /**
          * Constructs the Kleene star operation on an NFA
          * 
          * @param n the NFA which will be repeated
+         * @return the NFA formed by adding the Kleene Star operation
          */
         private NFA repeat(NFA n)
         {
@@ -111,7 +195,6 @@ public class RE implements REInterface {
                 n.addStartState(newStartName);
                 counter++;
                 String newFinalName = String.valueOf(counter);
-                NFAState newFinal = new NFAState(newFinalName);
                 n.addState(newFinalName);
                 counter++;
                 
@@ -119,11 +202,20 @@ public class RE implements REInterface {
                 n.addTransition(newStartName, 'e', oldStart.getName());
                 for (State s: n.getFinalStates()) {
                         ((NFAState) s).setNonFinal();
-                        ((NFAState) s).addTransition('e', newFinal);
+                        n.addTransition(s.getName(), 'e', newFinalName);
+                        n.addTransition(s.getName(), 'e', oldStart.getName());
                 }
                 n.addFinalState(newFinalName);
+                n.addTransition(newStartName, 'e', newFinalName);
                 return n;
         }
+        /**
+         * Performs the union operation on two NFAs and returns the result
+         * 
+         * @param n1 the top half of the union
+         * @param n2 the bottom half of the union
+         * @return the union of n1 and n2 as an NFA
+         */
         private NFA union(NFA n1, NFA n2)
         {
                 NFA n = new NFA();
@@ -131,13 +223,19 @@ public class RE implements REInterface {
                 counter++;
                 n.addStartState(newStartName);
                 
+                //Add transitions from old starts to the new one
                 ((NFAState)n.getStartState()).addTransition('e', (NFAState)n1.getStartState());
                 ((NFAState)n.getStartState()).addTransition('e', (NFAState)n2.getStartState());
                 
-                n1.addNFAStates(n1.getStates());
-                n1.addNFAStates(n2.getStates());
+                //Add states and alphabets from n1 and n2
+                n.addNFAStates(n1.getStates());
+                n.addNFAStates(n2.getStates());
+                n.addAbc(n1.getABC());
+                n.addAbc(n2.getABC());
                 
+                //Create new final state and make transitions from old final states
                 String newFinalName = String.valueOf(counter);
+                counter++;
                 n.addFinalState(newFinalName);
                 for (State s: n.getFinalStates()) {
                         if (s.getName().equals(newFinalName)) {
